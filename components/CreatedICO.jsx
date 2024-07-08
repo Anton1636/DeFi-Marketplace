cfbb97d5d511c0efd184af6165d6c0ffd59ce2e4
Hobermac <54035686+Anton1636@users.noreply.github.com>
2024-07-08 20:37:59 +0300

    Initial commit and created smart contract

---
+++ components/CreatedICO.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CreatedICO = () => {
+	return <div>CreatedICO</div>
+}
+
+export default CreatedICO
---
+++ components/WithdrawToken.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WithdrawToken = () => {
+	return <div>WithdrawToken</div>
+}
+
+export default WithdrawToken
